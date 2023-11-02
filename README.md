81cf69c3683f96677aae79e7b81ca6d3b7a59fbc
MrRobot0 <wietze@osch.net>
2023-11-02 16:37:32 +0100

    Fixed autoconnect

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ services:
         image: ghcr.io/mrrobot0/s7_securecontainer:master
         ports:
             - 8080:80
+        restart: unless-stopped
+    dockerproxy:
+        image: ghcr.io/tecnativa/docker-socket-proxy:latest
+        environment:
+            - CONTAINERS=1 # Allow access to viewing containers
+            - CONFIGS=1 # Allow access to viewing config of containers
+            - POST=0 # Disallow any POST operations (effectively read-only)
         volumes:
-            - /var/run/docker.sock:/var/run/docker.sock
+            - /var/run/docker.sock:/var/run/docker.sock:ro # Mounted as read-only
         restart: unless-stopped
 ```
--- S7_SecureContainer/Services/DockerService.cs
+++ S7_SecureContainer/Services/DockerService.cs
@@ -4,13 +4,40 @@ using Docker.DotNet.Models;
 using S7_SecureContainer.Models;
 using System.Net.Sockets;
 using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text.Json;
 
 namespace S7_SecureContainer.Services
 {
     public class DockerService
     {
-        public String ConnectionString { get; set; } = string.Empty;
+        private class Config
+        {
+            public string DockerHost { get; set; } = string.Empty;
+        }
+        public string ConnectionString { get; set; } = string.Empty;
+        public bool ManualHostInput { get; set; } = false;
         public DockerClient? Client { get; set; } = null;
+        public const string DefaultHost = "tcp:///dockerproxy";
+        public const string DefaultUnixHost = "unix:///var/run/docker.sock";
+        private readonly IToastService ToastService;
+        public DockerService (IToastService toastService)
+        {
+            ToastService = toastService;
+        }
+
+        public static string GetHostFromConfig()
+        {
+            const string configFile = "config.json";
+            try
+            {
+                string configData = File.ReadAllText(configFile);
+                Config? config = JsonSerializer.Deserialize<Config>(configData);
+                if (config != null) return config.DockerHost;
+            }
+            catch (Exception) { }
+            return string.Empty;
+        }
 
         public void Clear()
         {
@@ -18,13 +45,44 @@ namespace S7_SecureContainer.Services
             ConnectionString = string.Empty;
         }
 
+        public async Task<bool> TryAutoConnect()
+        {
+            if (await TryConnect(DefaultUnixHost)) 
+            {
+                ToastService.ShowToast(ToastLevel.Error, 
+                    string.Format("Please do not use a direct connection to docker host using: {0}",
+                                    DefaultUnixHost));
+                return false;
+            }
+            if (await TryConnect(DefaultHost)) { return true; }
+            if(await TryConnect(GetHostFromConfig())) { return true; }
+            ToastService.ShowToast(ToastLevel.Warning, "Could not auto-connect to a Docker host, please consult the docs.");
+            ToastService.ShowToast(ToastLevel.Info, "Reverting to manual host input..");
+            ManualHostInput = true;
+            return false;
+        }
+
+        private async Task<bool> TryConnect(string connectionString)
+        {
+            if (connectionString == string.Empty) { return false; }
+            List<ToastModel> toasts = await Connect(connectionString);
+            if (!toasts.Any(t => t.Level == ToastLevel.Error))
+            {
+                toasts.ForEach(t => ToastService.ShowToast(t.Level, t.Message));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         public async Task<List<ToastModel>> Connect(string connectionString)
         {
             List<ToastModel> toasts = new();
             if (connectionString == string.Empty)
             {
-                toasts.Add(new ToastModel(ToastLevel.Warning, "Please enter a value.."));
+                toasts.Add(new ToastModel(ToastLevel.Error, "Please enter a value.."));
                 return toasts;
             }
             DockerClient client;
