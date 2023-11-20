6a14842cde248cb0ffbc91584aa3d6120576434a
MrRobot0 <wietze@osch.net>
2023-11-20 23:08:00 +0100

    Refactor (#7)

---
+++ S7_SecureContainer/Models/Tests/CheckCPULimit.cs
@@ -0,0 +1,27 @@
+﻿using Docker.DotNet.Models;
+using S7_SecureContainer.Models.Docker;
+using S7_SecureContainer.Models.Test;
+using S7_SecureContainer.Services;
+
+namespace S7_SecureContainer.Models.Tests
+{
+	public static class CheckCPULimit
+	{
+		public static async Task<TestResult> Run(ContainerInspectResponse containerInspectResponse)
+		{
+			try
+			{
+				var cpu = containerInspectResponse.HostConfig.NanoCPUs;
+				if (cpu == 0)
+				{
+					return new TestResult(ContainerTestTypes.CPULimit, TestResult.Status.Warning, "It's recommended to limit CPUs used");
+				}
+				return new TestResult(ContainerTestTypes.CPULimit, TestResult.Status.Passed);
+			}
+			catch (Exception)
+			{
+				return new TestResult(ContainerTestTypes.CPULimit, TestResult.Status.Invalid);
+			}
+		}
+	}
+}
---
+++ S7_SecureContainer/Models/Tests/CheckForDefaultNetwork.cs
@@ -0,0 +1,32 @@
+﻿using Docker.DotNet.Models;
+using S7_SecureContainer.Models.Docker;
+using S7_SecureContainer.Models.Test;
+using S7_SecureContainer.Services;
+using System.Collections.Generic;
+
+namespace S7_SecureContainer.Models.Tests
+{
+	public static class CheckForDefaultNetwork
+	{
+		public static async Task<TestResult> Run(ContainerInspectResponse containerInspectResponse)
+		{
+			try
+			{
+				var networks = containerInspectResponse.NetworkSettings.Networks;
+
+				foreach (var network in networks)
+				{
+					if (network.Key == "bridge")
+					{		
+						return new TestResult(ContainerTestTypes.DefaultNetwork, TestResult.Status.Failed);
+					}
+				}
+				return new TestResult(ContainerTestTypes.DefaultNetwork, TestResult.Status.Passed);
+			}
+			catch (Exception)
+			{
+				return new TestResult(ContainerTestTypes.DefaultNetwork, TestResult.Status.Invalid);
+			}
+		}
+	}
+}
